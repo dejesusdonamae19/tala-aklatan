58395a331bdfbdfd42f99cbae645e9d738e55d62
nanamidonananami@gmail.com <verroyamayangelie@gmail.com>
2022-07-28 10:38:28 +0800

    this is an php file

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php include 'includes/session.php'; ?>
+
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-red layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	        <div class="row">
+	        	<div class="col-sm-8 col-sm-offset-2">
+	        		<?php
+	        			if(isset($_SESSION['error'])){
+	        				echo "
+	        					<div class='alert alert-danger'>
+	        						".$_SESSION['error']."
+	        					</div>
+	        				";
+	        				unset($_SESSION['error']);
+	        			}
+	        		?>
+	        	
+			        			</tbody>
+			        		</table>
+	        			</div>
+	        		</div>
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+<script>
+$(function(){
+	$('#catlist').on('change', function(){
+		if($(this).val() == 0){
+			window.location = 'index.php';
+		}
+		else{
+			window.location = 'index.php?category='+$(this).val();
+		}
+		
+	});
+});
+</script>
+</body>
+</html>
