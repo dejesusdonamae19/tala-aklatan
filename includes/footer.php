728a85b0eb68c9b815b3fd582c479d88bdab7c24
CatapangAshley <61414083+CatapangAshley@users.noreply.github.com>
2022-07-28 11:14:35 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        
+      </div>
+      <strong>&copy; 2022 - Tala-Aklatan | Team Quadrupleeee</a></strong>
+      </div>
+    <!-- /.container -->
+</footer>
