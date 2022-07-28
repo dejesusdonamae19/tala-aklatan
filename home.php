5e721f0afd56242602b3f42d119e75cad51c0443
dejesusdonamae19 <109118097+dejesusdonamae19@users.noreply.github.com>
2022-07-28 09:59:30 +0800

    Update home.php

--- home.php
+++ home.php
@@ -112,7 +112,7 @@ $(function(){
         label               : 'Borrow',
         fillColor           : 'rgba(210, 215, 222, 1)',
         strokeColor         : 'rgba(210, 214, 222, 1)',
-        pointColor          : 'rgba(210, 214, 222, 1)',
+        pointColor          : 'rgba(210, 215, 222, 1)',
         pointStrokeColor    : '#c1c7d1',
         pointHighlightFill  : '#fff',
         pointHighlightStroke: 'rgba(220,220,220,1)',
